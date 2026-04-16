f87b7eabfd5e6c5e446eb575465493d760d0b058
zefanya <zefanyatiomora@gmail.com>
2026-04-16 13:26:16 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardMahasiswaController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TesKemampuanController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// login
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// logout
+Route::get('/logout', [LoginController::class, 'logout']);
+
+// LANGSUNG KE DASHBOARD (sementara tanpa login)
+Route::get('/', [DashboardMahasiswaController::class, 'mahasiswa']);
+
+// Route dashboard
+Route::get('/dashboard-mahasiswa', [DashboardMahasiswaController::class, 'mahasiswa'])
+    ->name('dashboard.mahasiswa');
+Route::get('/tes-kemampuan', [TesKemampuanController::class, 'index'])
+    ->name('tes.kemampuan');
+Route::get('/tes-kemampuan/{id_area}', [TesKemampuanController::class, 'cluster'])
+    ->name('tes.kemampuan.cluster');
+Route::get('/tes-kemampuan/cluster/{id_cluster}', [TesKemampuanController::class, 'soal'])
+    ->name('tes.kemampuan.soal'); // ✅ INI YANG KURANG
+Route::post('/tes-kemampuan/submit', [TesKemampuanController::class, 'submit'])
+    ->name('tes.kemampuan.submit');
